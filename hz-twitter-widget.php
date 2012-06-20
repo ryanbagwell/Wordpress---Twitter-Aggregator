4a89b470927a5b931934c062022ea5416e0ce6ec
Ryan Bagwell <ryan@ryanbagwell.com>
2012-06-20 17:01:45 -0500

    added front-end loading to twitter feed widget

--- hz-twitter-widget.php
+++ hz-twitter-widget.php
@@ -26,100 +26,94 @@ class HZTwitterfeed extends WP_Widget {
 
 	}
 
-	function widget($args, $instance) {
+	function widget($args, $instance) { ?>
 		
-		//default settings		
-		$before_widget = "<li class='widget hz-twitter-feed-widget'>";
-		$after_widget = "</li>";
-		$before_title = "<h2>";
-		$after_title = "</h2>";
-		
-		$this->set_feed_urls($instance);
-				
-		echo $before_widget;
-		
-    	echo $before_title . $instance['title'] . $after_title;
-		
-		$xml = simplexml_load_string($this->get_combined_feed());
-      
-		$items = $xml->item;
+		<li class='widget hz-twitter-feed-widget'>
+			<h2><?php echo $instance['title']; ?></h2>
+			<ul class="tweets"></ul>
+			<div class='extra-1'></div>
+			<div class='extra-2'></div>
+			<div class='extra-3'></div>
+		</li>
+		<script type="text/javascript">
+			var options = {
+				maxItems: '<?php echo $instance['max_items']; ?>'
+			};
+			var months = [
+				"January",
+				"February",
+				"March",
+				"April",
+				"May",
+				"June",
+				"July",
+				"August",
+				"September",
+				"October",
+				"November",
+				"December"	
+			];
+			jQuery.post('/wp-admin/admin-ajax.php', {
+				action: 'hz_twitter_ajax',
+				feeds: <?php echo json_encode($instance['feeds']); ?>
+			}, function(response) {
+				$(response).find('status:lt(5)').each(function(i) {
+					var d = new Date($(this).find('created_at').first().text());
+					var tweetURL = $(this).find('sourceURL').text();
+					$('<li />').append(
+						$('<span />').addClass('tweet').text($(this).find('text').text()),
+						$('<a />').addClass('url').attr('href',tweetURL).text(tweetURL),
+						$('<span />').addClass('date').text(
+							[
+								months[d.getMonth()-1],d.getDate(),
+								', '
+								,d.getFullYear()
+							].join(' ')
+						),
+						$('<span />').addClass('time').text(
+							[
+								d.getHours() > 12 ? d.getHours() - 12 : d.getHours(),
+								':',
+								d.getMinutes(),
+								d.getHours() > 12 ? 'p.m.' : 'a.m.'
+							].join('')
+						),
+						$('<span />').addClass('via').text($(this).find('source').first().text())
+					).css('display','none').appendTo('ul.tweets').delay(500*i).slideDown(400);
+				});
+			},'xml');
+		</script>
 	
-		$max_items = $instance['max_items'];
-					
-		$i = 0;		
-		while($i<$max_items) { 
-
-			/*remove the twitter name prefix from the tweet that
-			for some reason Twitter started inserting into the rss feeds*/
-			$parts = explode(':',$items[$i]->title);
-			$tweet = ltrim($items[$i]->title,$parts[0].":");
-					
-			?>
-			<div class="tweet-wrapper">
-				<span class="tweet"><?php echo $tweet; ?></span>
-				<a class="url" rel="external" href="<?php echo $items[$i]->tinyUrl;?>"><?php echo $items[$i]->tinyUrl;?></a>
-				<div class="date-wrapper">
-					<span class="date"><?php echo date(get_site_option('date_format'),strtotime($items[$i]->pubDate));?></span>
-					<span class="time"> at <?php echo date(get_site_option('time_format'),strtotime($items[$i]->pubDate));?></span>
-					<span class="via">via <?php echo $items[$i]->children('http://api.twitter.com')->source; ?></span>
-				</div>
-			</div>
-		
-		<?php
-		$i++;	
-		}
-		
-		//empty divs for extra images or whatever
-		echo "<div class='extra-1'></div>";
-		echo "<div class='extra-2'></div>";
-		echo "<div class='extra-3'></div>";
-		
-		echo $after_widget;
-		
+	<?php	
+	
 	}
 
-	//puts all the urls from the widget settings into an array
-	function set_feed_urls($instance) {
+	function get_feed_items_html() {
 		
-		//get all the feeds and stick them in an array
-		foreach($instance as $key=>$value) {
+		$this->update_feed_file( $_POST['feeds'] );
 		
-			if (strpos($key,'tfeed-') !== false && $value != '') {
-				$feed_no = substr($key,8);			
-				
-				$this->feed_urls['tfeed-' . $feed_no] = $value;
-		
-			}
-		
-		}
-		
+		return file_get_contents(dirname(__FILE__).'/hz-twitter-feed.xml');
+			
 	}
 
 	//grabs the xml content from each feed and stores it in an array
-	function set_feed_content() {
-		foreach($this->feed_urls as $key => $url) {
-			$this->feed_content[$key] = file_get_contents($url);	
+	function set_feed_content( $feeds = array()) {
+		foreach($feeds as $feed) {
+			$this->feed_content[] = file_get_contents(str_replace(array('.rss', '.json'), '.xml', $feed));	
 		}	
 	}
 
-
-	function get_combined_feed() {
 	
-		//first, check if it's time to update the file
-		$feed = simplexml_load_file(dirname(__FILE__) . '/hz-twitter-feed.xml');
+	function update_feed_file( $feeds ) {
+
+		$this->feed_urls = $feeds;
 		
 		//if it's been less than 15 minutes since we last update the file, don't update it.
-		if (time() - $feed->lastUpdate > 900)
-			$this->update_feed_file();
-
-		return file_get_contents(dirname(__FILE__). '/hz-twitter-feed.xml');
-		
-	}
-	
-	function update_feed_file() {
+		if (time() - filemtime(dirname(__FILE__) . '/hz-twitter-feed.xml')  < 900)
+			return false;
 
 		//grab the latest feed urls
-		$this->set_feed_content();
+		$this->set_feed_content( $feeds );
 		
 		//set an array to sort the feed items by timeline
 		$sort_array = array();
@@ -127,91 +121,39 @@ class HZTwitterfeed extends WP_Widget {
 		//loop through the list of feeds and grab the content of each feed
 		foreach($this->feed_content as $feed_no => $feed) {
 			
-			$xml = simplexml_load_string($feed);
-			$items = $xml->channel->item;
+			$xml = simplexml_load_string( $feed );
 			
-			$i=0;
-			foreach($items as $item_no => $item) {
-					
-				$sort_array[$feed_no . '-' . $i] = strtotime($item->pubDate);
-			$i++;
+			foreach($xml->status as $item) {
+				$user = $item->user->screen_name;
+				$status_id = $item->id;
+				$url = "http://www.twitter.com/$user/statuses/$status_id";
+				$item->addChild('sourceURL', $this->get_tiny_url($url));
+				$sort_array[strtotime($item->created_at)] = $item;
 			}
 			
 		}
-				
 		//sort them based on the timestamp value
 		arsort($sort_array,SORT_NUMERIC);
-
-		//a placeholder for our namespaces so we can add them in later
-		$namespaces = array();
 		
 		//a placeholder for the xml string to add to the new xmldoc later
 		$xml_str = '';
 		
-		foreach($sort_array as $item => $timestamp) {
-			
-			$item = explode('-',$item);
-			$feed_no = $item[1];
-			$feed_item_no = (int)$item[2];
-						
-			$content = $this->feed_content['tfeed-' . $feed_no];
-					
-			$xml = simplexml_load_string($content);
-			
-			//we need to define all of the namespaces in our combined document so it doesn't throw an error
-			foreach($xml->getDocNamespaces() as $key => $ns) {
-				$namespaces[$key] = $ns;
-			};
-						
-			//for rss feed formats
-			$rss_items = $xml->channel->item;
-			if (count($rss_items) > 0)
-					$xml_str .= $rss_items[$feed_item_no]->asXML();
-	
-
-			//for atom formats
-			$atom_items = $xml->feed->entry[$feed_item_no];
-			if (count($atom_items) > 0)
-				$xml_str .= $atom_items->asXML();
-	
+		foreach($sort_array as $timestamp => $item) {
+			$xml_str .= $item->asXML();
 		}
-
-		//construct a string for namespaces
-		$ns_string = '';
-		foreach($namespaces as $key=>$value) {
-			$ns_string .= "xmlns:$key='$value' ";
-		}
-
-
+	
 		//put our xml string together and add  it to the 
-		$xml = "<xml $ns_string>";
-		$xml .= "<lastUpdate>" . time() . "</lastUpdate>";
-		$xml .= "$xml_str</xml>";
-
-
-		//add a tiny url to items
-		$xml = simplexml_load_string($xml);
-		$items = $xml->item;
-		
-		$i = 0;
-		foreach($items as $item) {
-			$item->addChild('tinyUrl',$this->get_tiny_url($item->link));
-			$xml->items[$i] = $item;
-		
-		$i++;	
-		}
-		
+		$xml = "<statusUpdates><lastUpdate>".time()."</lastUpdate>$xml_str</statusUpdates>";
+				
 		//write the new content to the file
 		$handle = fopen(dirname(__FILE__) . '/hz-twitter-feed.xml','w');
-		fwrite($handle,$xml->asXML());
+		fwrite($handle,$xml);
 		fclose($handle);
 		
 	}
 
 
-	function form($args) {
-				
-		?>
+	function form($args) { ?>
 
 		<div class="hz-field-wrapper">
 			<label for="<?php echo $this->get_field_id('title'); ?>">Title:</label>
@@ -219,34 +161,26 @@ class HZTwitterfeed extends WP_Widget {
 		</div>
 		
 		<div class="hz-feed-fields hzclearfix">
-
-			<div class="hz-field-wrapper">
-				
-				<label for="<?php echo $this->get_field_id('tfeed-1'); ?>">Feed 1:</label>
-				<input type="text" id="<?php echo $this->get_field_id('tfeed-1'); ?>" class="hz-feed-1" name="<?php echo $this->get_field_name('tfeed-1'); ?>" value="<?php echo $args['tfeed-1']; ?>" />
-			</div>
 			
 			<?php 
 			
-			$i = 1;
-			foreach($args as $key => $value) {
-				
-				if (strpos($key,'tfeed-1'))
-					continue;
-					
-				if (strpos($key,'tfeed-')) { ?>
-					 
-					<div class="hz-field-wrapper">
-						<label for="<?php echo $this->get_field_id("tfeed-$i"); ?>">Feed <?php echo $i; ?>:</label>
-						<input type="text" id="<?php echo $this->get_field_id("tfeed-$i"); ?>" class="hz-feed-<?php echo $i; ?>" name="<?php echo $this->get_field_name("tfeed-$i"); ?>" value="<?php echo $value; ?>" />
-					</div>
-					
-				<?php
-				}
-				$i++;
-			}
+			if ($args['feeds'] == '')
+				$args['feeds'] = array('');
 			
-			?>
+			$i = 1;
+			foreach($args['feeds'] as $value): ?>
+
+			<div class="hz-field-wrapper">
+				
+				<label for="<?php echo $this->get_field_id("feed$i"); ?>">Feed <?php echo $i; ?></label>
+				<input type="text" id="<?php echo $this->get_field_id("feed$i"); ?>" class="hz-feed-<?php echo $i; ?>" name=" <?php echo $this->get_field_name('feeds'); ?>[]" value="<?php echo $value; ?>" />
+				
+			</div>
+			
+			<?php 
+			$i++;
+			endforeach; ?>
+			
 		</div>
 		
 		<span class="hz-add-feed">Add another feed</span>
@@ -261,15 +195,19 @@ class HZTwitterfeed extends WP_Widget {
 	<?php }
 
 
-	function update($new_instance, $old_instance) {				
+	function update($new_instance, $old_instance) {
 
 		foreach($new_instance as $key => $value) {
 			
-			if($value == '') {
-			//if (strpos($key,'hz_feed_') >= 0 && $value == '') {
+			if ( $value == '' )
 				unset($new_instance[$key]);
-			} 
 			
+			if ( is_array($value) )
+				foreach( $value as $item => $feed) {
+					if ($feed == '')
+						unset($new_instance[$key][$item]);
+				}
+				
 		}
 		
 		return $new_instance;
@@ -285,8 +223,16 @@ class HZTwitterfeed extends WP_Widget {
 		return $tiny_url;
 	
 	}
+	
+	function hz_twitter_ajax() {
+		echo $this->get_feed_items_html();
+		die();
+	}
+	
 
 }
-			
+
+add_action('wp_ajax_nopriv_hz_twitter_ajax',create_function('', '$t = new HZTwitterfeed(); return $t->hz_twitter_ajax();'));	
+add_action('wp_ajax_hz_twitter_ajax',create_function('', '$t = new HZTwitterfeed(); return $t->hz_twitter_ajax();'));			
 add_action('widgets_init', create_function('', 'return register_widget("HZTwitterfeed");'));
 
--- hz-twitter.js
+++ hz-twitter.js
@@ -1,14 +1,8 @@
 jQuery(document).ready(function($) {
-
   $('.widgets-holder-wrap .hz-add-feed').live('click',function() {
-    var parent = $(this).parents('.widgets-holder-wrap');
-    var fields = $(parent).find('.hz-feed-fields input');
-    var fieldName = $(parent).find('.field-name-prefix').text();
-    fieldName = fieldName.replace('[]','[tfeed-' + parseInt(fields.length+1) + ']');
-    var class = "hz-feed-" + parseInt(fields.length+1);
-          
-    $(parent).find('.hz-feed-fields').append('<div class="hz-field-wrapper"><label>Feed ' + parseInt(fields.length + 1) + ':</label><input id="' + class + '"type="text" name="' + fieldName + '" value="" /></div>');
-
-	});  
-
+    var widget = $(this).parents('.widgets-holder-wrap');
+    var lastField = widget.find('.hz-feed-fields').children().last();
+    var newField = lastField.clone();
+    newField.find('label').text('Feed ' + (widget.find('.hz-feed-fields').children().length + 1) + ':').parent().find('input').val('').parent().insertAfter(lastField);
+	});
 });
